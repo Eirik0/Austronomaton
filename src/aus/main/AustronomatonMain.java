62588ecffba77a6b39d6c4eee17e6d90f9b158b7
Eirik Ogilvie-Wigley <eowigley@gmail.com>
2019-09-08 12:40:05 -0600

    game-template update

--- src/aus/main/AustronomatonMain.java
+++ src/aus/main/AustronomatonMain.java
@@ -1,8 +1,5 @@
 package aus.main;
 
-import java.awt.Dimension;
-
-import gt.component.ComponentCreator;
 import gt.component.GamePanel;
 import gt.component.MainFrame;
 import gt.gamestate.GameStateManager;
@@ -11,15 +8,11 @@ public class AustronomatonMain {
     private static final String TITLE = "Austronomaton";
 
     public static void main(String[] args) {
-        ComponentCreator.setCrossPlatformLookAndFeel();
-
-        GamePanel mainPanel = new GamePanel("Austronomaton");
-        mainPanel.setPreferredSize(new Dimension(ComponentCreator.DEFAULT_WIDTH, ComponentCreator.DEFAULT_HEIGHT));
+        MainFrame mainFrame = new MainFrame(TITLE);
+        GamePanel mainPanel = mainFrame.getGamePanel();
 
         GameStateManager gameStateManager = mainPanel.getGameStateManager();
         gameStateManager.setGameState(new SpaceGameState(gameStateManager.getMouseTracker()));
-
-        MainFrame mainFrame = new MainFrame(TITLE, mainPanel);
 
         mainFrame.show();
     }
